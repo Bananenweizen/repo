4f70b60576f3d7fdb3efe591a0269d138d0cb6d7
GerritSe <gerrit.seger@gmail.com>
2015-02-07 01:06:35 +0100

    Value2 hinzugefügt

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 class MainController
 {
 	private $config_value = 0;
+	private $config_value2 = 5;
 
 	public function MainController()
 	{
@@ -18,7 +19,18 @@ class MainController
 	{
 		return $this->value;
 	}
+
+	public function set_config_value2($value)
+	{
+		$this->config_value2 = value;
+	}
+
+	public function get_config_value2()
+	{
+		return $this->value2;
+	}
 }
 
 $main_controller = new MainController();
 $main_controller->run();
+print $main_controller->get_config_value2();
